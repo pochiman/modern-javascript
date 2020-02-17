3ae53c21678857ffa0a7006dcc2ff35f7850d860
pochiman <pochiman@yahoo.com>
2020-02-17 18:40:24 -0500

    21

--- control-flow-basics/sandbox.js
+++ control-flow-basics/sandbox.js
@@ -1,19 +1,7 @@
-// while loops
-const names = ['shaun', 'mario', 'luigi'];
-let i = 0;
+// do while loops
+let i = 5;
 
-// while(i < 5){
-//   console.log('loop: ', i);
-//   i++;
-// }
-
-// while(i < names.length){
-//   console.log(names[i]);
-//   i++;
-// }
-
-i = 8;
-while(i > 5){
-  console.log('loop: ', i);
-  i--;
-}
+do{
+  console.log('val of i is: ', i);
+  i++;
+} while(i < 5);
