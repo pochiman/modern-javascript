fbbd5f6e87984907d6ff2403605a6349c3ddd249
pochiman <pochiman@yahoo.com>
2020-02-18 18:30:28 -0500

    102

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,1 +1,18 @@
 const key = 'tNWM8nfwanGBUy3BpJ1zt7CdptZOEcUA';
+
+// get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+getCity('manchester')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
