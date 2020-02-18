b747db72cd2e76fbfa43eec6585dfcf51ef0b230
pochiman <pochiman@yahoo.com>
2020-02-18 17:18:12 -0500

    93

--- async-javascript/sandbox.js
+++ async-javascript/sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
 
   const request = new XMLHttpRequest();
 
@@ -13,16 +13,17 @@ const getTodos = (callback) => {
 
   });
   
-  request.open('GET', 'todos.json');
+  request.open('GET', resource);
   request.send();
 
 };
 
-getTodos((err, data) => {
-  console.log('callback function fired');
-  if(err){
-    console.log(err);
-  } else {
+getTodos('json/luigi.json', (err, data) => {
+  console.log(data);
+  getTodos('json/mario.json', (err, data) => {
     console.log(data);
-  }
+    getTodos('json/shaun.json', (err, data) => {
+      console.log(data);
+    });
+  });
 });
