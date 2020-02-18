18a9d199e0dc9eb1becb91145bd8966175be4e0a
pochiman <pochiman@yahoo.com>
2020-02-17 20:33:34 -0500

    39

--- object-literals/sandbox.js
+++ object-literals/sandbox.js
@@ -5,17 +5,20 @@ let user = {
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite']
+  blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+  login: function(){
+    console.log('the user logged in');
+  },
+  logout: function(){
+    console.log('the user logged out');
+  },
+  logBlogs: function(){
+    // access the blogs here
+  }
 };
 
-console.log(user);
-console.log(user.age);
+user.login();
+user.logout();
 
-user.age = 35;
-console.log(user.age);
-
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-console.log(typeof user);
+const name = 'shaun';
+name.toUpperCase();
