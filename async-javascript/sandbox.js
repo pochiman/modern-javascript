535c2194bca030746606c4a9efc075d414d032a3
pochiman <pochiman@yahoo.com>
2020-02-18 17:15:59 -0500

    92

--- async-javascript/sandbox.js
+++ async-javascript/sandbox.js
@@ -5,20 +5,18 @@ const getTodos = (callback) => {
   request.addEventListener('readystatechange', () => {
 
     if(request.readyState === 4 && request.status === 200){
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4){
       callback('could not fetch the data', undefined);
     }
 
   });
   
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.open('GET', 'todos.json');
   request.send();
 
 };
-
-console.log(1);
-console.log(2);
 
 getTodos((err, data) => {
   console.log('callback function fired');
@@ -28,6 +26,3 @@ getTodos((err, data) => {
     console.log(data);
   }
 });
-
-console.log(3);
-console.log(4);
