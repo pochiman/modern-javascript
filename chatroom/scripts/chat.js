77378cfaf55f8656bdaa152e7838fde5f08dd8f1
pochiman <pochiman@yahoo.com>
2020-02-19 11:46:23 -0500

    139

--- chatroom/scripts/chat.js
+++ chatroom/scripts/chat.js
@@ -22,10 +22,20 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+  getChats(callback){
+    this.chats
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if(change.type === 'added'){
+            callback(change.doc.data());
+          }
+        });
+    });
+  }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
 
-chatroom.addChat('hello everyone')
-  .then(() => console.log('chat added'))
-  .catch(err => console.log(err));
+chatroom.getChats(data => {
+  console.log(data);
+});
