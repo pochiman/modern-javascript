852ef7e1abd38b373ffe87648aa6c97a6f638575
pochiman <pochiman@yahoo.com>
2020-02-19 11:59:15 -0500

    141

--- chatroom/scripts/chat.js
+++ chatroom/scripts/chat.js
@@ -1,13 +1,9 @@
-// adding new chat documents
-// setting up a real-time listener to get new chats
-// updating the username
-// updating the room
-
 class Chatroom {
   constructor(room, username){
     this.room = room;
     this.username = username;
     this.chats = db.collection('chats');
+    this.unsub;
   }
   async addChat(message){
     // format a chat object
@@ -23,7 +19,7 @@ class Chatroom {
     return response;
   }
   getChats(callback){
-    this.chats
+    this.unsub = this.chats
       .where('room', '==', this.room)
       .orderBy('created_at')
       .onSnapshot(snapshot => {
@@ -34,6 +30,16 @@ class Chatroom {
         });
     });
   }
+  updateName(username){
+    this.username = username;
+  }
+  updateRoom(room){
+    this.room = room;
+    console.log('room updated');
+    if(this.unsub){
+      this.unsub();
+    }
+  }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
@@ -41,3 +47,11 @@ const chatroom = new Chatroom('gaming', 'shaun');
 chatroom.getChats(data => {
   console.log(data);
 });
+
+setTimeout(() => {
+  chatroom.updateRoom('general');
+  chatroom.updateName('yoshi');
+  chatroom.getChats(data => console.log(data));
+  chatroom.addChat('hello');
+}, 3000);
+
